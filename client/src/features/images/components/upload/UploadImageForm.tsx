8ebd7c36936f6021fa71ad4f94b0c3d87a995dba
Clay Dunston <claydunston@gmail.com>
2022-08-03 11:17:22 -0500

    [WIP] upload image / create image

--- client/src/features/images/components/upload/UploadImageForm.tsx
+++ client/src/features/images/components/upload/UploadImageForm.tsx
@@ -3,14 +3,20 @@ import Box from "@mui/material/Box"
 export const UploadImageForm = (_props: unknown): JSX.Element => {
     return (
         <Box>
-            <form action="http://localhost:3000/api/cloudflare/images/create" method="post">
-                <label htmlFor="fname">First name:</label>
+            <iframe name="hiddenFrame" style={{ display: "none", border: 0, width: 0, height: 0 }}></iframe>
+            <form
+                action="http://localhost:3000/api/cloudflare/images/create"
+                method="post"
+                encType="multipart/form-data"
+                target="hiddenFrame"
+            >
+                <label htmlFor="firstName">First name:</label>
                 <br/>
-                <input type="text" id="fname" name="fname"/>
+                <input type="text" id="firstName" name="firstName" value="John" />
                 <br/>
-                <label htmlFor="lname">Last name:</label>
+                <label htmlFor="lastName">Last name:</label>
                 <br/>
-                <input type="text" id="lname" name="lname"/>
+                <input type="text" id="lastName" name="lastName" value="Smith" />
                 <br/>
                 <hr/>
                 <br/>
